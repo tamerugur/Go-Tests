61e5240919d54bda2e0536912aa1e4775479c2fd
tamer ugur <tamerugur3@hotmail.com>
2025-07-17 14:23:44 +0300

    added convert method and used flags like -f, -c to convert celsius to fahrenheit, vice versa

--- main.go
+++ main.go
@@ -1,8 +1,36 @@
 package main
-import "fmt"
 
-func main(){
-	fmt.Println("Hello, go!")
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	convertCmd := flag.NewFlagSet("convert", flag.ExitOnError)
+	// define flags for fahrenheit and celsius, -f and -c, default to 0.0
+	// by default it returns a pointer to the flag
+	// ":=" is short variable declaration, it can only be used inside functions and type is inferred from right-hand side
+	f := convertCmd.Float64("f", 0.0, "Fahrenheit")
+	c := convertCmd.Float64("c", 0.0, "Celsius")
+
+	if len(os.Args) < 2 {
+		fmt.Println("please use subcommand")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "convert":
+		convertCmd.Parse(os.Args[2:])
+		if convertCmd.Parsed() {
+			if *f != 0 {
+				fmt.Println("°C:", (*f-32)*5/9)
+			} else if *c != 0 {
+				fmt.Println("°F:", (*c*9/5)+32)
+			}
+		}
+	default:
+		fmt.Println("unknown subcommand:", os.Args[1])
+		os.Exit(1)
+	}
 }
-
-
